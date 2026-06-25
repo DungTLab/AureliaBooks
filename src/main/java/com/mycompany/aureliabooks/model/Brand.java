1d0989291f1b13f24faa2e1ea61b912d12a97307
Le Tien Dung <ledungcom12@gmail.com>
2026-06-25 23:30:47 +0700

    feat: add entity models for Author, Brand, Inventory, Publisher, and Supplier; enhance list.jsp for admin book management (#15)

---
+++ src/main/java/com/mycompany/aureliabooks/model/Brand.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aureliabooks.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ * Brand Entity model.
+ * Created like NetBeans Maven template.
+ * @author DungLT
+ */
+public class Brand implements Serializable {
+    private int id;
+    private String name;
+    private String originCountry;
+    private Timestamp createdAt;
+
+    public Brand() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOriginCountry() {
+        return originCountry;
+    }
+
+    public void setOriginCountry(String originCountry) {
+        this.originCountry = originCountry;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
